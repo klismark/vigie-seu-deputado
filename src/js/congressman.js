4fb23b2cc67d6aff721b2fc2b625478ad3af5d93
Klismark Barbosa <klismark@hotmail.com>
2017-08-11 12:06:40 -0300

    Correção da execução de scripts em página errads.

--- src/js/congressman.js
+++ src/js/congressman.js
@@ -4,10 +4,9 @@ define(
         var actions = require('actions');
         var view = require('view');
            
-        
         //Atualiza os status dos objetos ARIA
         view.changeStatusWAIARIA();
-
+        
         //Acessibilidade
         actions.configAnnyangHome();
         actions.verifySpeechRecognition();
@@ -17,21 +16,9 @@ define(
         $("#btn-contrast").click(actions.changeStatusContrast);
         $('#btn-up-font').click(view.upFontSize);
         $('#btn-low-font').click(view.lowFontSize);
-
-        if(document.querySelector("#home")){       
-            //Lista os partidos no(s) select(s) de partidos.
-            actions.listAllCongressmen();
-            
-            //Ação do link "Todos os Deputados
-            $("#form-filter-congressmen").submit(actions.filterCongressmen);
-            $("#text-search").keyup(actions.filterCongressmen);
-            console.log("aaa");
-        }else{
-            console.log("bbb");
-            //Gráficos
-            view.buildCharts();
-        }
         
+        //Gráficos
+        view.buildCharts();
         
     }
 );
